c1c3b8379bd153da5bb222c363ac93d01a17186e
Ayon Lee <ayonlys@gmail.com>
2020-01-25 06:51:32 +0800

    add more accurate type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
-export declare function isArrayLike(obj: any): boolean;
-export declare function isCollectionLike(obj: any, excludeWeakOnes?: boolean): boolean;
-export declare function isBufferLike(obj: any): boolean;
-export declare function isErrorLike(obj: any): boolean;
-export declare function isPromiseLike(obj: any): boolean;
+export declare function isArrayLike(obj: any): obj is ArrayLike<any>;
+export declare function isCollectionLike(obj: any): boolean;
+export declare function isCollectionLike(obj: any, excludeWeakOnes: boolean): obj is {
+    size: number;
+} & Iterable<any>;
+export declare function isBufferLike(obj: any): obj is Pick<Buffer, "byteLength" | "slice">;
+export declare function isErrorLike(obj: any): obj is Error;
+export declare function isPromiseLike(obj: any): obj is PromiseLike<any>;
