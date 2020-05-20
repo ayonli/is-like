9e1149c5fa6a83ab9f342f0d6676ee4e3d759cf1
Ayon Lee <ayonlys@gmail.com>
2020-05-20 16:32:27 +0800

    fix the type of isBufferLike

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,6 @@ export declare function isCollectionLike(obj: any): boolean;
 export declare function isCollectionLike(obj: any, excludeWeakOnes: boolean): obj is {
     size: number;
 } & Iterable<any>;
-export declare function isBufferLike(obj: any): obj is Pick<Buffer, "byteLength" | "slice">;
+export declare function isBufferLike(obj: any): obj is Partial<Buffer> & Pick<Buffer, "byteLength" | "slice">;
 export declare function isErrorLike(obj: any): obj is Error;
 export declare function isPromiseLike(obj: any): obj is PromiseLike<any>;
